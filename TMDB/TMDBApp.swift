3d1657dca83556a22b2fde07ee871f75550d0f9d
Muhammad M. Munir <munir@bareksa.com>
2022-07-29 01:15:00 +0700

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Muhammad M Munir on 29/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
